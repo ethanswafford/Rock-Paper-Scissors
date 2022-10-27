a7fe6449ab9a9e90536fae39d3ed39f4073118d2
Ethan Swafford <ethanswafford@yahoo.com>
2022-10-27 00:24:09 -0500

    devPush

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const cpuSel = document.getElementById("cpu-select");
 const playSel = document.getElementById("player-select");
 const resultChoice = document.getElementById("results");
-const rockSel = document.getElementById("rock");
-const paperSel = document.getElementById("paper");
-const sciSel = document.getElementById("scissors");
+const gameChoices = document.querySelectorAll("button");
+
+gameChoices.forEach(button => gameChoices.addEventListener('click', () => {
+
+}))
