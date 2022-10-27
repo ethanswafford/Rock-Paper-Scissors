a1cb26dc528f16f2cfd1ef2f5772f9eac8a4af08
Ethan Swafford <ethanswafford@yahoo.com>
2022-10-27 01:51:02 -0500

    devPush

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const cpuSel = document.getElementById("cpu-select");
 const playSel = document.getElementById("player-select");
-const resultChoice = document.getElementById("results");
+const gameResult = document.getElementById("results");
 const gameChoices = document.querySelectorAll("button");
 let userChoice;
 let cpuChoice;
+let result;
 
 
 gameChoices.forEach(button => button.addEventListener('click', (e) => {
@@ -16,13 +17,25 @@ function cpuGenChoice() {
     const rng = Math.floor(Math.random() * gameChoices.length)
 
     if (rng === 0) {
-        cpuChoice = 'ROCK'
+        cpuChoice = ' ROCK'
     }
     if (rng === 1) {
-        cpuChoice = 'PAPER'
+        cpuChoice = ' PAPER'
     }
     if (rng === 2) {
-        cpuChoice = 'SCISSORS'
+        cpuChoice = ' SCISSORS'
     }
     cpuSel.innerHTML = cpuChoice
+}
+
+function getResults() {
+    if (cpuChoice === userChoice) {
+        result = " DRAW! THIS GAME WAS A PUSH."
+    }
+    if (cpuChoice === ' ROCK' && userChoice === ' PAPER') {
+        result = "COVERED AND SMOTHERED, PAPER BEATS ROCK, YOU WIN!"
+    }
+    if (cpuChoice === ' ROCK' && userChoice === ' SCISSORS') {
+        result = "BASHED AND SMASHED, ROCK CRUSHES SCISSORS, YOU LOSE!"
+    }
 }
