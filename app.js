47ab8a36bfe983cfdabd703065eb8f27300553e8
Ethan Swafford <ethanswafford@yahoo.com>
2022-10-27 01:15:59 -0500

    devPush

--- app.js
+++ app.js
@@ -3,16 +3,26 @@ const playSel = document.getElementById("player-select");
 const resultChoice = document.getElementById("results");
 const gameChoices = document.querySelectorAll("button");
 let userChoice;
+let cpuChoice;
 
 
 gameChoices.forEach(button => button.addEventListener('click', (e) => {
     userChoice = e.target.id
     playSel.innerHTML = userChoice
-    cpuChoice()
+    cpuGenChoice()
 }))
 
-function cpuChoice() {
+function cpuGenChoice() {
     const rng = Math.floor(Math.random() * gameChoices.length)
-    console.log(rng)
-    cpuSel.innerHTML = rng.toString()
+
+    if (rng === 0) {
+        cpuChoice = 'ROCK'
+    }
+    if (rng === 1) {
+        cpuChoice = 'PAPER'
+    }
+    if (rng === 2) {
+        cpuChoice = 'SCISSORS'
+    }
+    cpuSel.innerHTML = cpuChoice
 }
