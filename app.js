299e094f69d7692acfd3f8116b1522404a3ed330
Ethan Swafford <ethanswafford@yahoo.com>
2022-10-26 23:54:30 -0500

    devPush

---
+++ app.js
@@ -0,0 +1,6 @@
+const cpuSel = document.getElementById("cpu-select");
+const playSel = document.getElementById("player-select");
+const resultChoice = document.getElementById("results");
+const rockSel = document.getElementById("rock");
+const paperSel = document.getElementById("paper");
+const sciSel = document.getElementById("scissors");
